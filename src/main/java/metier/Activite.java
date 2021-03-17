9b5a2e63adea3718e23a2eb0d97d4b4182c995c1
Amandine <amandine.leroy@soprasteria.com>
2021-03-17 14:56:17 +0100

    Test activité

--- src/main/java/metier/Activite.java
+++ src/main/java/metier/Activite.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-
+//test
 @Entity
 public class Activite {
 	
