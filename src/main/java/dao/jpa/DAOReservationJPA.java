3a2b612a8db460c9de49e293a465cb90664b14a8
Anaelle <anaelle.stum@soprasteria.com>
2021-03-17 14:03:35 +0100

    daoreservation voyage voyageur

--- src/main/java/dao/jpa/DAOReservationJPA.java
+++ src/main/java/dao/jpa/DAOReservationJPA.java
@@ -1,5 +1,69 @@
 package dao.jpa;
 
-public class DAOReservationJPA {
+import java.util.List;
+//test
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import dao.IDAOReservation;
+import metier.Reservation;
+import util.Context;
+
+public class DAOReservationJPA implements IDAOReservation {
+
+	@Override
+	public Reservation findById(Integer id) {
+		EntityManager em = Context.getInstance().getEmf().createEntityManager();
+		Reservation reservation = em.find(Reservation.class, id);
+		em.close();
+		return reservation;
+	}
+
+	@Override
+	public List<Reservation> findAll() {
+		EntityManager em = Context.getInstance().getEmf().createEntityManager();
+		Query myQuery = em.createQuery("SELECT c from Reservation c",Reservation.class);
+		 return myQuery.getResultList(); 
+	}
+
+	@Override
+	public Reservation save(Reservation reservation) {
+		EntityManager em = Context.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();		
+		reservation=em.merge(reservation);
+		
+		em.getTransaction().commit();
+		em.close();
+		return reservation;
+	}
+
+	@Override
+	public void delete(Reservation reservation) {
+		
+		EntityManager em = Context.getInstance().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		
+		reservation=em.merge(reservation);
+		em.remove(reservation);
+		
+		em.getTransaction().commit();
+		em.close();
+		
+	}
+
+	@Override
+	public void insert(Reservation d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Reservation d) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 
 }
